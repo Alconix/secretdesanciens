63065d65f663be366d63ec94599a7619ce2354ee
Nicolas Loyau <nicolas.loyau@epita.fr>
2019-12-05 23:51:03 +0100

    Add class list on recruitement homepage

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -240,23 +240,156 @@ const Home = () => {
           <b>Recrutement</b>
         </h1>
         <Box className='has-text-justified is-size-4'>
-          <ul>
-            <li>
-              Recrutement Tank:&nbsp;
-              <span className='has-text-danger'>Faible</span>
-            </li>
-            <li>
-              Recrutement Heal: <span className='has-text-success'>Fort</span>
-            </li>
-            <li>
-              Recrutement DPS: <span className='has-text-warning'>Moyen</span>
-            </li>
-          </ul>
           <div>
-            Pour créer une candidature, créez un compte avec le bouton
-            <i> Login</i> puis cliquez sur le bouton <i>Candidatures </i>
-            dans la barre de navigation.
+            <br />
+            <p>
+              Merci de lire la <b>Charte</b> avant de postuler.
+            </p>
+            <br />
+            <p>
+              Essayez d'avoir équipement un minimum en adéquation avec le
+              contenu visé avant de postuler, merci.
+            </p>
+            <br />
+            <p>
+              Pour créer une candidature, créez un compte avec le bouton
+              <i> Login</i> puis cliquez sur le bouton <i>Candidatures </i>
+              dans la barre de navigation.
+            </p>
           </div>
+          <br />
+          <table className='table is-hoverable' style={{ margin: "0 auto" }}>
+            <thead>
+              <tr>
+                <th>Classe</th>
+                <th>Tank</th>
+                <th>Heal</th>
+                <th>DPS</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td style={{ color: "#A330C9" }}>Chasseur de démons</td>
+                <td>
+                  <span className='has-text-success'>Faible</span>
+                </td>
+                <td>/</td>
+                <td>
+                  <span className='has-text-danger has-text-centered'>
+                    Fort
+                  </span>
+                </td>
+              </tr>
+              <tr>
+                <td style={{ color: "#C41F3B" }}>Chevalier de la mort</td>
+                <td>
+                  <span className='has-text-success'>Faible</span>
+                </td>
+                <td>/</td>
+                <td>
+                  <span className='has-text-warning'>Moyen</span>
+                </td>
+              </tr>
+              <tr>
+                <td style={{ color: "#8787ED" }}>Démoniste</td>
+                <td>/</td>
+                <td>/</td>
+                <td>
+                  <span className='has-text-danger'>Fort</span>
+                </td>
+              </tr>
+              <tr>
+                <td style={{ color: "#0070DE" }}>Chaman</td>
+                <td>/</td>
+                <td>
+                  <span className='has-text-warning'>Moyen</span>
+                </td>
+                <td>
+                  <span className='has-text-warning'>Moyen</span>
+                </td>
+              </tr>
+              <tr>
+                <td style={{ color: "#FF7D0A" }}>Druide</td>
+                <td>
+                  <span className='has-text-success'>Faible</span>
+                </td>
+                <td>
+                  <span className='has-text-warning'>Moyen</span>
+                </td>
+                <td>
+                  <span className='has-text-warning'>Moyen</span>
+                </td>
+              </tr>
+              <tr>
+                <td style={{ color: "#40C7EB" }}>Mage</td>
+                <td>/</td>
+                <td>/</td>
+                <td>
+                  <span className='has-text-danger'>Fort</span>
+                </td>
+              </tr>
+              <tr>
+                <td style={{ color: "#00FF96" }}>Moine</td>
+                <td>
+                  <span className='has-text-success'>Faible</span>
+                </td>
+                <td>
+                  <span className='has-text-warning'>Moyen</span>
+                </td>
+                <td>
+                  <span className='has-text-danger'>Fort</span>
+                </td>
+              </tr>
+              <tr>
+                <td style={{ color: "#F58CBA" }}>Paladin</td>
+                <td>
+                  <span className='has-text-success'>Faible</span>
+                </td>
+                <td>
+                  <span className='has-text-success'>Faible</span>
+                </td>
+                <td>
+                  <span className='has-text-warning'>Moyen</span>
+                </td>
+              </tr>
+              <tr>
+                <td style={{ color: "#FFFFFF" }}>Prêtre</td>
+                <td>/</td>
+                <td>
+                  <span className='has-text-warning'>Moyen</span>
+                </td>
+                <td>
+                  <span className='has-text-warning'>Moyen</span>
+                </td>
+              </tr>
+              <tr>
+                <td style={{ color: "#FFF569" }}>Voleur</td>
+                <td>/</td>
+                <td>/</td>
+                <td>
+                  <span className='has-text-warning'>Moyen</span>
+                </td>
+              </tr>
+              <tr>
+                <td style={{ color: "#A9D271" }}>Chasseur</td>
+                <td>/</td>
+                <td>/</td>
+                <td>
+                  <span className='has-text-warning'>Moyen</span>
+                </td>
+              </tr>
+              <tr>
+                <td style={{ color: "#C79C6E" }}>Guerrier</td>
+                <td>
+                  <span className='has-text-success'>Faible</span>
+                </td>
+                <td>/</td>
+                <td>
+                  <span className='has-text-warning'>Moyen</span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </Box>
       </>
     );
